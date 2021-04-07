39227ab6c0ec3e3b3ae363f7dc99538ff24bb163
mvprawin <prawin.mattool07@gmail.com>
2021-04-07 10:08:50 +0530

    Initial commit

---
+++ src/test/java/com/qa/test/TestRunner.java
@@ -0,0 +1,26 @@
+package com.qa.test;
+
+
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.*;
+import cucumber.api.junit.Cucumber;
+
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	    features="src/test/resources/Features",
+	    glue="stepDefinitions",
+	   // plugin= {"pretty","html:target/site/cucumber-report-html","json:target/cucumber-report.json"},
+	   plugin= {"pretty","html:target/cucumber","json:target/cucumber.json",
+			   "com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/extent-reports/report.html"},
+	    monochrome=true
+	    )
+
+
+public class TestRunner {
+
+}
